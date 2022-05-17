91fccd4bf520e14a041aefac3b788d08630d65d9
Pranshu Chittora <pranshu@signoz.io>
2022-05-17 17:19:40 +0530

    feat: Comment and create Docs issue

---
+++ constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pr_number: process.env.PR_NUMBER,
+    REPO_OWNER: 'signoz',
+    REPO_SIGNOZ_CODE: 'signoz',
+    REPO_SIGNOZ_DOCS: 'signoz.io',
+    BOT_DOCS_COMMENT: 'For maintainers only\r\n- [ ] Update docs for this PR on signoz.io',
+    BOT_DOCS_COMMENT_CHECKED: 'For maintainers only\r\n- [x] Update docs for this PR on signoz.io',
+}
